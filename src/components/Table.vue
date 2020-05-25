27b85dbe2bd462ded6a546fbca27c249f1e5283b
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-05-25 11:35:11 -0400

    getting the table ready

---
+++ src/components/Table.vue
@@ -0,0 +1,39 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>Column1</th>
+                <th>Column2</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    Value1
+                </td>
+                <td>
+                    Value2
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Value1
+                </td>
+                <td>
+                    Value2
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+   name: "Table",
+   data: function() {
+       return {
+           
+       }
+   } 
+}
+</script>
