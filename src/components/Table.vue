d3ed0d6a6b82a44ca6bac11f89549aae44103fd0
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-05-25 13:47:18 -0400

    Update Table.vue
    working table with pagination

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,104 +3,115 @@
     <table>
         <thead>
             <tr>
-                <th>Column1</th>
-                <th>Column2</th>
+                <th>Date</th>
+                <th>Season</th>
+                <th>Team 1</th>
+                <th>Score 1</th>
+                <th>Team 2</th>
+                <th>Score 2</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <td>
-                    Value1
-                </td>
-                <td>
-                    Value2
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    Value1
-                </td>
-                <td>
-                    Value2
-                </td>
+            <tr v-for="object in tableRows" v-bind:key="object.id">
+                <td>{{object.date}}</td>
+                <td>{{object.season}}</td>
+                <td>{{object.team1}}</td>
+                <td>{{object.score1}}</td>
+                <td>{{object.team2}}</td>
+                <td>{{object.score2}}</td>
+                <!-- <td v-for="value in object" v-bind:key="value.date">{{value}}</td> -->
             </tr>
         </tbody>
     </table>
         <ul class="pagination">
-            <li class="page-item"><a href="#" class="page-link">prev</a></li>
-            <li class="page-item" v-for="i in pagesArray" v-bind:key="i.name"><a href="#" class="page-link">{{ i.name }}</a></li>
-            <li class="page-item"><a href="#" class="page-link">next</a></li>
+            <li class="page-item" v-on:click="prevPage()"><a href="#" class="page-link">prev</a></li>
+            <li class="page-item" v-for="i in pagesArray" v-bind:key="i.name" v-on:click="changePage(i.name)"><a href="#" class="page-link">{{ i.name }}</a></li>
+            <li class="page-item" v-on:click="nextPage()"><a href="#" class="page-link">next</a></li>
         </ul>
 </div>
 </template>
 
 <script>
 export default {
-   name: "Table",
-   data: function() {
-       return {
-           tableData: null,
-           tableMsg: null,
-           url: "http://127.0.0.1:5000/api/records/nba_elo/?season=2018",
-           maxVisibleButtons: 5,
-           pagesCurrent: 1,
-           perPage: 5,
-       }
+    name: "Table",
+    data: function() {
+        return {
+            tableData: [],
+            tableMsg: null,
+            url: "http://127.0.0.1:5000/api/records/nba_elo/?season=2018",
+            maxVisibleButtons: 5,
+            pagesCurrent: 1,
+            perPage: 10,
+        }
+    },
+    computed: {
+        pagesTotal() {
+            if (this.tableData != null) {
+                return Math.ceil(this.tableData.length / this.perPage)
+            } else {
+                return 0
+            }
+        },
+        pagesStart() {
+            if (this.pagesCurrent === 1) {
+                return 1
+            }
+
+            if (this.pagesCurrent === 2) {
+                return this.pagesCurrent - 1
+            }
+
+            if (this.pagesCurrent === this.pagesTotal) {
+                if ((this.pagesTotal - this.maxVisibleButtons + 1) <= 0) {
+                    return 1
+                }
+
+                return this.pagesTotal - this.maxVisibleButtons + 2
+            }
+
+            return this.pagesCurrent - 2
+        },
+        pagesEnd() {
+            return Math.min(this.pagesStart + this.maxVisibleButtons - 1, this.pagesTotal);
+        },
+        pagesArray() {
+            const range = []
+            for (let i = this.pagesStart; i <= this.pagesEnd; i++) {
+                range.push({
+                    name: i,
+                    isDisabled: i === this.pagesCurrent
+                })
+            }
+            return range
+        },
+        tableRows() {
+            let start = (this.pagesCurrent - 1) * parseInt(this.perPage)
+            let end = start + parseInt(this.perPage)
+            return this.tableData.slice(start, end)
+        },
+    },
+    mounted() {
+        this.setupTable()
+    },
+    methods: {
+        setupTable() {
+            this.$http.get(this.url).then(result => {
+                this.tableData = result.data
+            })
+            .catch(err => {
+                this.tableMsg = err
+            })
+        },
+        changePage(page) {
+            this.pagesCurrent = page
+        },
+        nextPage() {
+            if (this.pagesCurrent < this.pagesTotal) { this.pagesCurrent++ }
+        },
+        prevPage() {
+            if (this.pagesCurrent > 1) { this.pagesCurrent-- }
+        }
    },
-   computed: {
-       pagesTotal() {
-           if (this.tableData != null) {
-               return Math.ceil(this.tableData.length / this.perPage)
-           } else {
-               return 0
-           }
-       },
-       pagesStart() {
-           if (this.pagesCurrent === 1) {
-               return 1
-           }
-
-           if (this.pagesCurrent === 2) {
-               return this.pagesCurrent - 1
-           }
-
-           if (this.pagesCurrent === this.pagesTotal) {
-               if ((this.pagesTotal - this.maxVisibleButtons + 1) <= 0) {
-                   return 1
-               }
-
-               return this.pagesTotal - this.maxVisibleButtons + 2
-           }
-
-           return this.pagesCurrent - 2
-       },
-       pagesEnd() {
-           return Math.min(this.pagesStart + this.maxVisibleButtons - 1, this.pagesTotal);
-       },
-       pagesArray() {
-           const range = []
-           for (let i = this.pagesStart; i <= this.pagesEnd; i++) {
-               range.push({
-                   name: i,
-                   isDisabled: i === this.pagesCurrent
-               })
-           }
-           return range
-       },
-   },
-   mounted() {
-       this.setupTable()
-   },
-   methods: {
-       setupTable() {
-           this.$http.get(this.url).then(result => {
-               this.tableData = result.data
-           })
-           .catch(err => {
-               this.tableMsg = err
-           })
-       }
-   }
 }
 </script>
 
