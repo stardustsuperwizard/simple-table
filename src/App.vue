6fb5be7cf23af509f4d706de79fe47d39b1010e3
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2020-12-28 01:47:06 -0500

    updates and changes

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,12 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Table />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Table from './components/Table.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Table,
-  }
-}
-</script>
 
 <style>
 #app {
@@ -25,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
